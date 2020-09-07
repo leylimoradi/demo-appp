18feac7fbada61f6a7be4de3a231c0e50a7db3e0
Leyli Moradi <l.moradi@Gan.local>
2020-09-07 14:47:09 +0430

    Updates

---
+++ precache-manifest.a4328b215d83dfaa30c72903ec8bed5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4300555b25bc002642c27bbd9f69f14a",
+    "url": "/demo-appp/index.html"
+  },
+  {
+    "revision": "0c02c10d8896d4967028",
+    "url": "/demo-appp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ac0184666dc72fb79a4",
+    "url": "/demo-appp/static/js/2.4250e7d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-appp/static/js/2.4250e7d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c02c10d8896d4967028",
+    "url": "/demo-appp/static/js/main.9988c6c8.chunk.js"
+  },
+  {
+    "revision": "3dce6c029af7c3a1b9d0",
+    "url": "/demo-appp/static/js/runtime-main.2b2b660d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-appp/static/media/logo.5d5d9eef.svg"
+  }
+]);
